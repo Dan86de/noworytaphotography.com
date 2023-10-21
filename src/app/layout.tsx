6713d78240f26da71df77bd0f0d8852bfd1ef546
Daniel Noworyta <daniel.noworyta@gmail.com>
2023-10-21 21:28:57 +0200

    Initial shadcnui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,25 @@
-import type { Metadata } from 'next'
-import './globals.css'
+import type { Metadata } from "next";
+import "./globals.css";
+import { Montserrat } from "next/font/google";
+import { cn } from "@/lib/utils";
 
+export const montserrat = Montserrat({
+	subsets: ["latin"],
+	variable: "--font-sans",
+	weight: ["300", "400", "500"],
+});
 
 export const metadata: Metadata = {
-  title: 'Noworytaphotography - Katarzyna Noworyta',
-  description: 'Interior photographer Katarzyna Noworyta',
-}
+	title: "Noworytaphotography - Katarzyna Noworyta",
+	description: "Interior photographer Katarzyna Noworyta",
+};
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return (
-    <html lang="de">
-      <body >{children}</body>
-    </html>
-  )
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+	return (
+		<html lang="de" suppressHydrationWarning>
+			<body className={cn("bg-background min-h-screen font-sans antialiased", montserrat.variable)}>
+				{children}
+			</body>
+		</html>
+	);
 }
